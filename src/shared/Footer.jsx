22e88fa4e22b946a2f25b7ab34da66a95412f6e6
Shah Arefin Ahmed <shaharefinahmed@gmail.com>
2025-03-29 14:55:44 +0600

    Feat:Footer only responsive mobile has been done

--- src/shared/Footer.jsx
+++ src/shared/Footer.jsx
@@ -1,72 +1,72 @@
-import { Link } from 'react-router-dom';
-import logo from '../assets/images/logo.png';
-import { Facebook } from 'lucide-react';
+import { Link } from "react-router-dom";
+import logo from "../assets/images/logo.png";
+import { Facebook } from "lucide-react";
 import {
   FacebookSvg,
   InstagramSvg,
   TwitterSvg,
-} from '@/components/svg-container/SvgContainer';
+} from "@/components/svg-container/SvgContainer";
 
 const Footer = () => {
   const exploreData = [
     {
-      title: 'Home',
-      path: '/',
+      title: "Home",
+      path: "/",
     },
     {
-      title: 'Share recipe',
-      path: '/share-recipe',
+      title: "Share recipe",
+      path: "/share-recipe",
     },
     {
-      title: 'Meal planner',
-      path: '/meal-planner',
+      title: "Meal planner",
+      path: "/meal-planner",
     },
     {
-      title: 'Reviews',
-      path: '/reviews',
+      title: "Reviews",
+      path: "/reviews",
     },
     {
-      title: 'Blog',
-      path: '/blgo',
+      title: "Blog",
+      path: "/blgo",
     },
   ];
   const recipeLibraryData = [
     {
-      title: 'Keto diet',
-      path: '/keto-diet',
+      title: "Keto diet",
+      path: "/keto-diet",
     },
     {
-      title: 'Mediterranean Diet',
-      path: '/mediterranean-diet',
+      title: "Mediterranean Diet",
+      path: "/mediterranean-diet",
     },
     {
-      title: 'Vegan Diet',
-      path: '/vegan-diet',
+      title: "Vegan Diet",
+      path: "/vegan-diet",
     },
     {
-      title: 'Paleo Diet',
-      path: '/paleo-diet',
+      title: "Paleo Diet",
+      path: "/paleo-diet",
     },
   ];
 
   const socialLinks = [
     {
       svg: <FacebookSvg />,
-      path: '/',
+      path: "/",
     },
     {
       svg: <InstagramSvg />,
-      path: '/',
+      path: "/",
     },
     {
       svg: <TwitterSvg />,
-      path: '/',
+      path: "/",
     },
   ];
   return (
-    <footer className="bg-[#3A3A3A] pt-20">
+    <footer className="bg-[#3A3A3A] md:pt-20 pt-5">
       {/* top section */}
-      <section className="container text-white w-full flex justify-between pb-10">
+      <section className="container text-white w-full flex flex-col gap-5 md:gap-0 md:flex-row justify-between pb-10 px-6">
         <div className="space-y-2">
           <img src={logo} alt="" />
           <p className="max-w-[330px]">
@@ -75,9 +75,9 @@ const Footer = () => {
           </p>
         </div>
 
-        <div>
-          <h5 className="text-lg font-medium">Explore</h5>
-          <ul className="space-y-3 mt-4">
+        <div className="">
+          <h5 className="md:text-lg text-base md:font-medium">Explore</h5>
+          <ul className="md:space-y-3 space-y-2 md:mt-4 mt-2">
             {exploreData?.map((item) => (
               <li key={item?.title}>
                 <Link>{item?.title}</Link>
@@ -87,8 +87,10 @@ const Footer = () => {
         </div>
 
         <div>
-          <h5 className="text-lg font-medium">Recipe library</h5>
-          <ul className="space-y-3 mt-4">
+          <h5 className="md:text-lg text-base md:font-medium">
+            Recipe library
+          </h5>
+          <ul className="md:space-y-3 space-y-2 md:mt-4 mt-2">
             {recipeLibraryData?.map((item) => (
               <li key={item?.title}>
                 <Link>{item?.title}</Link>
@@ -102,7 +104,7 @@ const Footer = () => {
 
           {/* links */}
           <ul className="mt-4 flex items-center gap-4">
-            {socialLinks?.map((item,idx) => (
+            {socialLinks?.map((item, idx) => (
               <li key={idx}>
                 <Link>{item?.svg}</Link>
               </li>
@@ -113,7 +115,9 @@ const Footer = () => {
 
       {/* bottom section */}
       <section>
-        <p className='text-[#D0D0D0] text-center py-5 border-t border-primary'>© Mohammed Al-Hasani - All rights reserved</p>
+        <p className="text-[#D0D0D0] text-center py-5 border-t border-primary">
+          © Mohammed Al-Hasani - All rights reserved
+        </p>
       </section>
     </footer>
   );
