cd02d04bde57f9fe5ad3f32bbb18f4eaa838af77
Shah Arefin Ahmed <shaharefinahmed@gmail.com>
2025-04-01 11:21:12 +0600

    Feat:Blog page all device responsive has been done

--- src/pages/blog/Blog.jsx
+++ src/pages/blog/Blog.jsx
@@ -1,15 +1,15 @@
-import BlogCard from '@/components/cards/BlogCard';
-import { allBlogs } from '@/data/data';
+import BlogCard from "@/components/cards/BlogCard";
+import { allBlogs } from "@/data/data";
 
 const Blog = () => {
   return (
     <div className="mt-[104px]">
       {/* title */}
-      <div className="pt-20 pb-16 text-center">
-        <h2 className="text-5xl font-bold font-merriweather text-black">
+      <div className="xl:pt-20 lg:pt-14 md:pt-10 pt-14 xl:pb-16 lg:pb-8 text-center">
+        <h2 className="lg:text-3xl xl:text-5xl md:text-3xl text-2xl font-bold font-merriweather text-black">
           Recipe Blogs & Tips
         </h2>
-        <p className="mt-8 text-textColor leading-[150%] max-w-[912px] mx-auto">
+        <p className="xl:mt-8 mt-4 text-textColor leading-[150%] lg:max-w-[912px] mx-auto text-sm lg:text-base px-3 md:px-5 lg:px-0">
           A revolutionary approach to meal planning. Cutting-edge organization
           tools crafted to save time and tailor your weekly menu to your dietary
           preferences and habits.
@@ -17,7 +17,7 @@ const Blog = () => {
       </div>
 
       {/* all blogs */}
-      <div className="grid grid-cols-4 gap-5 container pb-20">
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 container pb-10 lg:pb-20 px-5 md:px-0 lg:px-0 mt-5 lg:mt-0">
         {allBlogs?.map((item) => (
           <BlogCard key={item?.id} data={item} />
         ))}
