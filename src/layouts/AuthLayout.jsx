b1c25be567eb919325c4c066b85c4f50260a3d01
Shah Arefin Ahmed <shaharefinahmed@gmail.com>
2025-04-02 10:03:18 +0600

    Feat:Register page all device responsive has been completed

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -2,8 +2,8 @@ import { Outlet } from "react-router-dom";
 
 const AuthLayout = () => {
   return (
-    <div className="min-h-screen max-h-screen flex items-center justify-center">
-      <div className="bg-[#FCFCFC] sm:p-8 sm:min-w-[600px]">
+    <div className="min-h-screen max-h-screen flex items-center justify-center px-5 sm:px-0">
+      <div className="bg-[#FCFCFC] sm:p-8 sm:min-w-[600px] w-full">
         <Outlet />
       </div>
     </div>
