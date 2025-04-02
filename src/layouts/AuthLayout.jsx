685f8055d12a48fc77a892288ae3267a46c6d104
Shah Arefin Ahmed <shaharefinahmed@gmail.com>
2025-04-02 10:05:01 +0600

    Fix:width related issues has been fixed

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 const AuthLayout = () => {
   return (
     <div className="min-h-screen max-h-screen flex items-center justify-center px-5 sm:px-0">
-      <div className="bg-[#FCFCFC] sm:p-8 sm:min-w-[600px] w-full">
+      <div className="bg-[#FCFCFC] sm:p-8 sm:min-w-[600px] w-[400px]">
         <Outlet />
       </div>
     </div>
