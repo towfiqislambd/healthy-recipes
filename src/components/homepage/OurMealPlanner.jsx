5108d81a7e5328d59abe627db0eb1ab550999862
tofiqsoftvance <tofiqulislam.softvance@gmail.com>
2025-04-08 14:06:59 +0600

    done

--- src/components/homepage/OurMealPlanner.jsx
+++ src/components/homepage/OurMealPlanner.jsx
@@ -2,17 +2,17 @@ import mealPlannerImage from "../../assets/images/meal-planner.png";
 import ButtonPrimary from "../buttons/ButtonPrimary";
 const OurMealPlanner = () => {
   return (
-    <section className="sm:py-24 bg-[#FCFCFC]">
+    <section className="py-7 md:py-12 2xl:py-16 3xl:py-20 bg-[#FCFCFC]">
       <div className="container">
-        <div className="flex flex-col xl:flex-row items-center gap-12 3xl:gap-32 lg:px-3 xl:px-5 2xl:px-10 3xl:px-0">
+        <div className="flex flex-col xl:flex-row items-center gap-8 xl:gap-12 3xl:gap-32 lg:px-3 xl:px-5 2xl:px-10 3xl:px-0">
           {/* left contents */}
           <div className="flex-1">
-            <h4 className="font-merriweather text-3xl 3xl:text-[40px] leading-[140%] font-bold">
+            <h4 className="font-merriweather text-2xl 2xl:text-3xl 3xl:text-[40px] leading-[140%] font-bold">
               Our Meal Planner
             </h4>
 
             {/* description */}
-            <div className="sm:space-y-6 space-y-3 mt-5 3xl:mt-8">
+            <div className="space-y-3 2xl:space-y-6 mt-3 2xl:mt-5 3xl:mt-8">
               <p className="text-textColor leading-[150%]">
                 Our Meal Planner tool is designed to make healthy eating
                 effortless and personalized for every age group. Whether you're
@@ -34,15 +34,15 @@ const OurMealPlanner = () => {
             </div>
 
             {/* explore more */}
-            <div className="sm:mt-10 mt-5">
+            <div className="2xl:mt-10 mt-5">
               <ButtonPrimary title="Explore more" />
             </div>
           </div>
           {/* right contents */}
-          <div className="bg-[#FDE0B8] sm:px-12 px-6 sm:py-14 py-7 rounded-lg flex-1 group overflow-hidden">
+          <div className="bg-[#FDE0B8] px-6 sm:px-12 sm:py-14 py-7 rounded-lg flex-1 group overflow-hidden w-full">
             <div>
               <img
-                className="group-hover:scale-105 transition-all duration-500 rounded-md"
+                className="group-hover:scale-105 transition-all duration-500 rounded-md w-full"
                 src={mealPlannerImage}
                 alt=""
               />
