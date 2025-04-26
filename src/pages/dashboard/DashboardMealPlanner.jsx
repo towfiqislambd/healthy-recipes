731e0911e8932fd2201918c7709127392a6334a4
tofiqsoftvance <tofiqulislam.softvance@gmail.com>
2025-04-26 15:53:44 +0600

    done

--- src/pages/dashboard/DashboardMealPlanner.jsx
+++ src/pages/dashboard/DashboardMealPlanner.jsx
@@ -2,41 +2,22 @@ import { ThreeDotSvg } from '@/components/svg-container/SvgContainer';
 import { useState } from 'react';
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover"
 import { Link } from 'react-router-dom';
-import { useAllCategories, useMealPlannerTable } from '@/hooks/cms.queries';
 import { useDeleteMealPlan } from '@/hooks/cms.mutations';
 import { Loader } from '@/components/loader/Loader';
 import Modal from '@/components/modals/Modal';
 import EditMealModal from '@/components/modals/EditMealModal';
+import { useMealPlannerTable } from '@/hooks/cms.queries';
 
 const DashboardMealPlanner = () => {
-    const [selectedMonth, setSelectedMonth] = useState('');
     const [itemId, setItemId] = useState('')
     const [open, setOpen] = useState(false);
     const [mealPlannerId, setMealPlannerId] = useState('');
-    const [activeTab, setActiveTab] = useState({ id: 0, category_name: 'All Recipes' });
-    const { data: allCategories, isLoading: categoryLoading } = useAllCategories();
-    const { data: mealPlannerTableData, isLoading: mealPlannerLoading } = useMealPlannerTable(activeTab?.id, selectedMonth);
+    const { data: mealPlannerTableData, isLoading } = useMealPlannerTable();
     const { mutateAsync: deleteMealPlan } = useDeleteMealPlan(mealPlannerId);
 
-    if (categoryLoading || mealPlannerLoading) {
+    if (isLoading) {
         return <div className="flex justify-center items-center h-[85vh]"><Loader /></div>;
     }
-
-    const monthData = [
-        { id: 0, month: 'All' },
-        { id: 1, month: 'January' },
-        { id: 2, month: 'February' },
-        { id: 3, month: 'March' },
-        { id: 4, month: 'April' },
-        { id: 5, month: 'May' },
-        { id: 6, month: 'June' },
-        { id: 7, month: 'July' },
-        { id: 8, month: 'August' },
-        { id: 9, month: 'September' },
-        { id: 10, month: 'October' },
-        { id: 11, month: 'November' },
-        { id: 12, month: 'December' },
-    ];
 
     // Delete Meal Plan
     const handleDeletePlan = async (meal_plan_id) => {
@@ -54,35 +35,11 @@ const DashboardMealPlanner = () => {
 
     return (
         <section className="3xl:p-5">
-            <h3 className="3xl:mb-7 text-xl 2xl:text-2xl text-[#E48E19] font-semibold font-merriweather">
-                Customize your meal plan
-            </h3>
-
-            {/* Tabs */}
-            <div className="py-5 2xl:py-8 w-full flex flex-wrap items-center justify-center 2xl:justify-between gap-x-1 gap-y-2">
-
-                <button
-                    onClick={() => setActiveTab({ id: 0, category_name: 'All Recipes' })}
-                    className={`px-3 sm:px-4 3xl:px-6 py-[5px] text-[15px] sm:text-base sm:py-2 3xl:py-3 rounded-full font-medium ${activeTab?.category_name === 'All Recipes'
-                        ? 'bg-[#3A3A3A] text-white'
-                        : 'bg-transparent text-textColor'
-                        }`}
-                >
-                    All Recipes
-                </button>
-
-                {allCategories?.map((tab) => (
-                    <button
-                        key={tab.id}
-                        onClick={() => setActiveTab(tab)}
-                        className={`px-3 sm:px-4 3xl:px-6 py-[5px] text-[15px] sm:text-base sm:py-2 3xl:py-3 rounded-full font-medium ${tab?.category_name === activeTab?.category_name
-                            ? 'bg-[#3A3A3A] text-white'
-                            : 'bg-transparent text-textColor'
-                            }`}
-                    >
-                        {tab?.category_name}
-                    </button>
-                ))}
+            <div className="3xl:mb-7 flex justify-between items-center">
+                <h3 className="text-xl 2xl:text-2xl text-[#E48E19] font-semibold font-merriweather">
+                    Customize your meal plan
+                </h3>
+                <Link to='/meal-planner'><button className='px-3 py-1.5 text-[#5A5C5F] border border-primary rounded-lg'>Add meal</button></Link>
             </div>
 
             {/* Table */}
@@ -91,19 +48,7 @@ const DashboardMealPlanner = () => {
                     <thead>
                         <tr className="text-[#5A5C5F] text-lg text-center font-merriweather text-nowrap">
                             <th className="bg-[#FCBD66] w-[150px] py-1 sm:py-2 4xl:px-3">
-                                <select
-                                    className="border border-[#FCBD66] rounded-[5px] px-3 py-2 text-sm sm:text-base sm:py-3 bg-transparent outline-none block w-full"
-                                    value={selectedMonth}
-                                    onChange={(e) => setSelectedMonth(Number(e.target.value))}
-                                >
-                                    {monthData?.map((item, idx) => (
-                                        <option key={idx} value={item?.id}>
-                                            {item?.month}
-                                        </option>
-                                    ))}
-                                </select>
-
-
+                                All
                             </th>
                             <th
                                 colSpan={7}
